be4021b21a0862eb8d8f7ce152448124c34dfcbe
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-22 06:07:06 +0600

    implement user registration, create the user in the database, Hash the pasword before saving to the database, create the user's profile in the database, fetch the created user alon with the profile to return in the response

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import express, { Application, Request, Response } from "express";
 import config from "./config";
+import httpStatus from "http-status";
+import bcrypt from "bcryptjs";
 import { prisma } from "./lib/prisma";
 
 const app: Application = express();
@@ -18,6 +20,68 @@ app.use(
 
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
+});
+
+// TODO: Implement user registration
+app.post("/api/users/register", async (req: Request, res: Response) => {
+  const { name, email, password, profilePhoto } = req.body;
+
+  // Check if user already exists
+  const isUserExist = await prisma.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (isUserExist) {
+    throw new Error("User already exists");
+  }
+
+  // Hash the password before saving to the database
+  const hashedPassword = await bcrypt.hash(
+    password,
+    Number(config.bcrypt_salt_rounds),
+  );
+
+  // Create the user in the database
+  const createdUser = await prisma.user.create({
+    data: {
+      name,
+      email,
+      password: hashedPassword,
+    },
+  });
+
+  // Create the user's profile in the database
+  await prisma.profile.create({
+    data: {
+      userId: createdUser.id,
+      profilePhoto,
+    },
+  });
+
+  // Fetch the created user along with the profile to return in the response
+  const user = await prisma.user.findUnique({
+    where: {
+      id: createdUser.id,
+      email: createdUser.email || email,
+    },
+    omit: {
+      password: true,
+    },
+    include: {
+      profile: true,
+    },
+  });
+
+  res.status(httpStatus.CREATED).json({
+    success: true,
+    statusCode: httpStatus.CREATED,
+    message: "User registration successful",
+    data: {
+      user,
+    },
+  });
 });
 
 export default app;
