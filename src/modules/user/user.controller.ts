7678270dc496e844494d4d0cacaf6b31ab8be095
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-26 11:34:15 +0600

    create a middler for me route controler and update cookie, verifiedToken. and role base system

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -3,9 +3,6 @@ import httpStatus from "http-status";
 import { userService } from "./user.service";
 import { catchAsync } from "../../utils/catchAsync";
 import { sendResponse } from "../../utils/sendResponse";
-import Jwt from "jsonwebtoken";
-import { jwtUtils } from "../../utils/jwt";
-import config from "../../config";
 
 const registerUser = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -24,18 +21,9 @@ const registerUser = catchAsync(
 
 const getMyProfile = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
-    const { accessToken } = req.cookies;
-
-    const verifiedToken = jwtUtils.verifyToken(
-      accessToken,
-      config.jwt_access_secret,
+    const profile = await userService.getMyProfileFromDB(
+      req.user?.id as string,
     );
-
-    if (typeof verifiedToken === "string") {
-      throw new Error(verifiedToken);
-    }
-
-    const profile = await userService.getMyProfileFromDB(verifiedToken.id);
 
     sendResponse(res, {
       success: true,
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,10 +1,64 @@
-import { Router } from "express";
+import { NextFunction, Request, Response, Router } from "express";
 import { userController } from "./user.controller";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
+import { Role } from "../../../generated/prisma/enums";
+import HttpStatus from "http-status";
 
 const router = Router();
 
+declare global {
+  namespace Express {
+    interface Request {
+      user?: {
+        email: string;
+        name: string;
+        id: string;
+        role: Role;
+      };
+    }
+  }
+}
+
 router.post("/register", userController.registerUser);
 
-router.get("/me", userController.getMyProfile);
+router.get(
+  "/me",
+  (req: Request, res: Response, next: NextFunction) => {
+    const { accessToken } = req.cookies;
+
+    const verifiedToken = jwtUtils.verifyToken(
+      accessToken,
+      config.jwt_access_secret,
+    );
+
+    if (typeof verifiedToken === "string") {
+      throw new Error(verifiedToken);
+    }
+
+    const { email, name, id, role } = verifiedToken;
+
+    const requiredRoles = [Role.ADMIN, Role.USER, Role.AUTHOR];
+
+    if (!requiredRoles.includes(role)) {
+      return res.status(403).json({
+        success: false,
+        statusCode: HttpStatus.FORBIDDEN,
+        message:
+          "Forbidden. You don't have permission to access this resource.",
+      });
+    }
+
+    req.user = {
+      email,
+      name,
+      id,
+      role,
+    };
+
+    next();
+  },
+  userController.getMyProfile,
+);
 
 export const userRoutes = router;
