fc4013f958f9b7ce3fc3858416fc8fe671480e34
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-26 12:46:59 +0600

    finaly update my profile and project done

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -34,7 +34,24 @@ const getMyProfile = catchAsync(
   },
 );
 
+const updateMyProfile = catchAsync(
+  async (req: Request, res: Response, next: NextFunction) => {
+    const userId = req.user?.id as string;
+    const payload = req.body;
+
+    const updatedProfile = await userService.updateMyProfile(userId, payload);
+
+    sendResponse(res, {
+      success: true,
+      statusCode: httpStatus.OK,
+      message: "User profile updated successfully",
+      data: { updatedProfile },
+    });
+  },
+);
+
 export const userController = {
   registerUser,
   getMyProfile,
+  updateMyProfile,
 };
--- src/modules/user/user.route.ts
+++ src/modules/user/user.route.ts
@@ -1,24 +1,23 @@
-import { NextFunction, Request, Response, Router } from "express";
+import { Router } from "express";
 import { userController } from "./user.controller";
-import config from "../../config";
-import { jwtUtils } from "../../utils/jwt";
 import { Role } from "../../../generated/prisma/enums";
-import HttpStatus from "http-status";
-import { catchAsync } from "../../utils/catchAsync";
-import { JwtPayload } from "jsonwebtoken";
-import { prisma } from "../../lib/prisma";
 import { auth } from "../../middlewares/auth";
 
 const router = Router();
 
 router.post("/register", userController.registerUser);
 
-auth(Role.ADMIN, Role.USER, Role.AUTHOR);
-
+// profile route
 router.get(
   "/me",
   auth(Role.ADMIN, Role.USER, Role.AUTHOR),
   userController.getMyProfile,
+);
+
+router.put(
+  "/my-profile",
+  auth(Role.ADMIN, Role.USER, Role.AUTHOR),
+  userController.updateMyProfile,
 );
 
 export const userRoutes = router;
