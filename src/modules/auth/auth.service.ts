06869d385dd2a8c6facd0446c4ed71ef67f7f369
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-26 07:37:49 +0600

    create accessToken and refreshToken with jwt secret

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,6 +1,9 @@
 import bcrypt from "bcryptjs";
 import { prisma } from "../../lib/prisma";
 import { ILoginUser } from "./auth.types";
+import jwt, { SignOptions } from "jsonwebtoken";
+import config from "../../config";
+import { jwtUtils } from "../../utils/jwt";
 
 const loginUser = async (payload: ILoginUser) => {
   const { email, password } = payload;
@@ -15,7 +18,32 @@ const loginUser = async (payload: ILoginUser) => {
     throw new Error("Password is incorrect");
   }
 
-  return user;
+  // for accessToken and refreshToken
+  const jwtPayload = {
+    id: user.id,
+    name: user.name,
+    email: user.email,
+    role: user.role,
+  };
+
+  // accessToken
+  const accessToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_access_secret,
+    config.jwt_access_expires_in as SignOptions,
+  );
+
+  // refreshToken
+  const refreshToken = jwtUtils.createToken(
+    jwtPayload,
+    config.jwt_refresh_secret,
+    config.jwt_refresh_expires_in as SignOptions,
+  );
+
+  return {
+    accessToken,
+    refreshToken,
+  };
 };
 
 export const authService = {
