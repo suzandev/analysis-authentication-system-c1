e52b78693d2d9a7d457d78ee189ea2e42acab7eb
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-29 12:08:11 +0600

    update getPostById with Transaction and Rollback

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -29,47 +29,49 @@ const getAllPosts = async () => {
 };
 
 const getPostById = async (postId: string) => {
-  await prisma.post.update({
-    where: {
-      id: postId,
-    },
-    data: {
-      views: {
-        increment: 1,
+  const transactionResult = await prisma.$transaction(async (tx) => {
+    await tx.post.update({
+      where: {
+        id: postId,
       },
-    },
+      data: {
+        views: {
+          increment: 1,
+        },
+      },
+    });
+
+    const post = await tx.post.findUniqueOrThrow({
+      where: {
+        id: postId,
+      },
+      include: {
+        author: {
+          omit: {
+            password: true,
+          },
+        },
+        comments: {
+          where: {
+            status: CommentStatus.APPROVED,
+          },
+          orderBy: {
+            createdAt: "desc",
+          },
+        },
+
+        _count: {
+          select: {
+            comments: true,
+          },
+        },
+      },
+    });
+
+    return post;
   });
 
-  //throw new Error("Fake Error");
-
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: postId,
-    },
-    include: {
-      author: {
-        omit: {
-          password: true,
-        },
-      },
-      comments: {
-        where: {
-          status: CommentStatus.APPROVED,
-        },
-        orderBy: {
-          createdAt: "desc",
-        },
-      },
-
-      _count: {
-        select: {
-          comments: true,
-        },
-      },
-    },
-  });
-
-  return post;
+  return transactionResult;
 };
 
 const updatePost = async (
