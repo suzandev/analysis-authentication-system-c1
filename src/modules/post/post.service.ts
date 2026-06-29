e12232fa4770f1fa6474505a8aafc4c08d68c954
Suzan Chandra <suzanchandra@Suzans-Mac-mini.local>
2026-06-29 11:25:04 +0600

    update getPostById fild

--- src/modules/post/post.service.ts
+++ src/modules/post/post.service.ts
@@ -1,3 +1,4 @@
+import { CommentStatus } from "../../../generated/prisma/enums";
 import { prisma } from "../../lib/prisma";
 import { ICreatePostPayload, IUpdatePostPayload } from "./post.interface";
 
@@ -28,13 +29,7 @@ const getAllPosts = async () => {
 };
 
 const getPostById = async (postId: string) => {
-  const post = await prisma.post.findUniqueOrThrow({
-    where: {
-      id: postId,
-    },
-  });
-
-  const updatedPost = await prisma.post.update({
+  await prisma.post.update({
     where: {
       id: postId,
     },
@@ -43,17 +38,38 @@ const getPostById = async (postId: string) => {
         increment: 1,
       },
     },
+  });
+
+  //throw new Error("Fake Error");
+
+  const post = await prisma.post.findUniqueOrThrow({
+    where: {
+      id: postId,
+    },
     include: {
       author: {
         omit: {
           password: true,
         },
       },
-      comments: true,
+      comments: {
+        where: {
+          status: CommentStatus.APPROVED,
+        },
+        orderBy: {
+          createdAt: "desc",
+        },
+      },
+
+      _count: {
+        select: {
+          comments: true,
+        },
+      },
     },
   });
 
-  return updatedPost;
+  return post;
 };
 
 const updatePost = async (
